5d68bdcfd2b46e49b19aafd616c70bdd6eaf7b00
Wes Payne <noblepayne@noblepayne.com>
2025-09-22 16:34:48 -0700

    feat: add nix tooling for formatting and deployment

--- flake.nix
+++ flake.nix
@@ -1,10 +1,8 @@
 {
   description = "Dawarich points collector";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
   };
-
   outputs = {
     self,
     nixpkgs,
@@ -12,12 +10,10 @@
     supportedSystems = ["x86_64-linux" "aarch64-linux"];
     pkgsBySystem = nixpkgs.lib.getAttrs supportedSystems nixpkgs.legacyPackages;
     forAllPkgs = fn: nixpkgs.lib.mapAttrs (_: pkgs: fn pkgs) pkgsBySystem;
-
     gitRev =
       if self ? rev
       then builtins.substring 0 8 self.rev
       else "dirty";
-
     mkCollector = pkgs:
       pkgs.writeShellApplication {
         name = "collector";
@@ -34,7 +30,6 @@
           platforms = supportedSystems;
         };
       };
-
     mkImage = pkgs: tag:
       pkgs.dockerTools.buildImage {
         name = "collector";
@@ -53,17 +48,28 @@
       };
   in {
     formatter = forAllPkgs (pkgs: pkgs.alejandra);
-
     overlays.default = final: prev: {
       collector = mkCollector final;
     };
-
     packages = forAllPkgs (pkgs: {
       collector = (self.overlays.default pkgs pkgs).collector;
       collector-image-latest = mkImage pkgs "latest";
       collector-image-gitrev = mkImage pkgs gitRev;
       default = self.packages.${pkgs.system}.collector-image-gitrev;
-      deployIndex = pkgs.writeShellApplication {
+
+      # Format index.html with prettier
+      format-index = pkgs.writeShellApplication {
+        name = "format-index";
+        runtimeInputs = [
+          pkgs.nodePackages.prettier
+        ];
+        text = ''
+          prettier -w index.html
+        '';
+      };
+
+      # Deploy index.html
+      deploy-index = pkgs.writeShellApplication {
         name = "deploy-index";
         runtimeInputs = [
           pkgs.s5cmd
@@ -75,6 +81,35 @@
           s5cmd --endpoint-url="$AWS_ENDPOINT_URL" cp index.html "s3://$R2_BUCKET/index.html"
         '';
       };
+
+      # Deploy with dotenvx wrapper
+      deploy-with-env = pkgs.writeShellApplication {
+        name = "deploy-with-env";
+        runtimeInputs = [
+          pkgs.s5cmd
+          pkgs.dotenvx
+        ];
+        text = ''
+          dotenvx run -- ${self.packages.${pkgs.system}.deploy-index}/bin/deploy-index
+        '';
+      };
+
+      # Combined format and deploy
+      format-and-deploy = pkgs.writeShellApplication {
+        name = "format-and-deploy";
+        runtimeInputs = [
+          pkgs.nodePackages.prettier
+          pkgs.s5cmd
+          pkgs.dotenvx
+        ];
+        text = ''
+          echo "Formatting index.html..."
+          prettier -w index.html
+
+          echo "Deploying index.html..."
+          dotenvx run -- ${self.packages.${pkgs.system}.deploy-index}/bin/deploy-index
+        '';
+      };
     });
 
     apps = forAllPkgs (pkgs: {
@@ -84,6 +119,49 @@
         meta = {
           description = "Run the Dawarich collector.";
         };
+      };
+      format-index = {
+        type = "app";
+        program = pkgs.lib.getExe self.packages.${pkgs.system}.format-index;
+        meta = {
+          description = "Format index.html with prettier.";
+        };
+      };
+      deploy-with-env = {
+        type = "app";
+        program = pkgs.lib.getExe self.packages.${pkgs.system}.deploy-with-env;
+        meta = {
+          description = "Deploy index.html with dotenvx env injection.";
+        };
+      };
+      format-and-deploy = {
+        type = "app";
+        program = pkgs.lib.getExe self.packages.${pkgs.system}.format-and-deploy;
+        meta = {
+          description = "Format and deploy index.html in one command.";
+        };
+      };
+    });
+
+    devShells = forAllPkgs (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodePackages.prettier
+          pkgs.dotenvx
+          pkgs.s5cmd
+        ];
+        shellHook = ''
+          echo "Texas Tracker dev environment"
+          echo "Available commands:"
+          echo "  prettier -w index.html       - Format HTML"
+          echo "  dotenvx run -- <command>     - Run with .env"
+          echo "  s5cmd                         - S3 operations"
+          echo ""
+          echo "Or use nix run commands:"
+          echo "  nix run .#format-index"
+          echo "  nix run .#deploy-with-env"
+          echo "  nix run .#format-and-deploy"
+        '';
       };
     });
   };
